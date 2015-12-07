005cea3407ecbf3df01ea6d927ca2e464466c2b9
Adam Chambers <adammchambers@gmail.com>
2015-12-06 23:12:58 -0500

    Added support for jsonp counts and Pinterest

--- src/modules/count.js
+++ src/modules/count.js
@@ -4,7 +4,6 @@
 module.exports = class Count {
 
     constructor(type, url) {
-
         // throw error if invalid type provided
         if (!this[type]) {
             throw new Error(`Open Share: ${type} is an invalid count type`);
@@ -15,12 +14,40 @@ module.exports = class Count {
             throw new Error(`Open Share: no url provided for count`);
         }
 
+        // store count URL and transform function
         this.countData = this[type](url);
     }
 
+    // fetch count either AJAX or JSONP
     getCount(os) {
+        if (this.countData.jsonp) {
+            this.jsonp(os);
+        } else {
+            this.get(os);
+        }
+    }
+
+    // handle JSONP requests
+    jsonp(os) {
+        // define random callback and assign transform function
+        let callback = `jsonp_${Math.random().toString().substr(-10)}`;
+        window[callback] = (data) => {
+            os.innerHTML = this.countData.transform(data);
+        };
+
+        // append JSONP script tag to page
+        let script = document.createElement('script');
+        script.src = this.countData.url.replace('callback=?', `callback=${callback}`);
+        document.getElementsByTagName('head')[0].appendChild(script);
+
+        return;
+    }
+
+    // handle AJAX request
+    get(os) {
         let xhr = new XMLHttpRequest();
 
+        // on success pass response to transform function
         xhr.onreadystatechange = () => {
             if (xhr.readyState !== XMLHttpRequest.DONE ||
                 xhr.status !== 200) {
@@ -34,11 +61,23 @@ module.exports = class Count {
         xhr.send();
     }
 
+    // facebook count data
     facebook(url) {
         return {
             url: `http://graph.facebook.com/?id=${url}`,
             transform: function(xhr) {
                 return JSON.parse(xhr.responseText).shares;
+            }
+        };
+    }
+
+    // pinterest count data
+    pinterest(url) {
+        return {
+            jsonp: true,
+            url: `http://api.pinterest.com/v1/urls/count.json?callback=?&url=${url}`,
+            transform: function(data) {
+                return data.count;
             }
         };
     }
