06d9ee34aadb21a6df861be41f2f5dd08a5af4cb
Adam Chambers <adammchambers@gmail.com>
2016-01-05 14:15:18 +0000

    Update linting to new Boilerplate style

--- src/modules/count.js
+++ src/modules/count.js
@@ -1,198 +1,198 @@
 /**
-* OpenShare generates a single share link
-*/
+ * OpenShare generates a single share link
+ */
 module.exports = class Count {
 
-    constructor(type, url) {
-        // throw error if invalid type provided
-        if (!this[type]) {
-            throw new Error(`Open Share: ${type} is an invalid count type`);
-        }
+	constructor(type, url) {
+		// throw error if invalid type provided
+		if (!this[type]) {
+			throw new Error(`Open Share: ${type} is an invalid count type`);
+		}
 
-        // throw error if no url provided
-        if (!url) {
-            throw new Error(`Open Share: no url provided for count`);
-        }
+		// throw error if no url provided
+		if (!url) {
+			throw new Error(`Open Share: no url provided for count`);
+		}
 
-        this.type = type;
+		this.type = type;
 
-        // store count URL and transform function
-        this.countData = this[type](url);
-    }
+		// store count URL and transform function
+		this.countData = this[type](url);
+	}
 
-    // fetch count either AJAX or JSONP
-    getCount(os) {
-        var count = this.storeGet(this.type);
+	// fetch count either AJAX or JSONP
+	getCount(os) {
+		var count = this.storeGet(this.type);
 
-        if (count) {
-            os.innerHTML = count;
-        }
+		if (count) {
+			os.innerHTML = count;
+		}
 
-        this[this.countData.type](os);
-    }
+		this[this.countData.type](os);
+	}
 
-    // handle JSONP requests
-    jsonp(os) {
-        // define random callback and assign transform function
-        let callback = `jsonp_${Math.random().toString().substr(-10)}`;
-        window[callback] = (data) => {
-            let count = this.countData.transform(data);
-            os.innerHTML = count;
-        };
+	// handle JSONP requests
+	jsonp(os) {
+		// define random callback and assign transform function
+		let callback = `jsonp_${Math.random().toString().substr(-10)}`;
+		window[callback] = (data) => {
+			let count = this.countData.transform(data);
+			os.innerHTML = count;
+		};
 
-        // append JSONP script tag to page
-        let script = document.createElement('script');
-        script.src = this.countData.url.replace('callback=?', `callback=${callback}`);
-        document.getElementsByTagName('head')[0].appendChild(script);
+		// append JSONP script tag to page
+		let script = document.createElement('script');
+		script.src = this.countData.url.replace('callback=?', `callback=${callback}`);
+		document.getElementsByTagName('head')[0].appendChild(script);
 
-        return;
-    }
+		return;
+	}
 
-    // handle AJAX GET request
-    get(os) {
-        let xhr = new XMLHttpRequest();
+	// handle AJAX GET request
+	get(os) {
+		let xhr = new XMLHttpRequest();
 
-        // on success pass response to transform function
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState !== XMLHttpRequest.DONE ||
-                xhr.status !== 200) {
-                return;
-            }
+		// on success pass response to transform function
+		xhr.onreadystatechange = () => {
+			if (xhr.readyState !== XMLHttpRequest.DONE ||
+				xhr.status !== 200) {
+				return;
+			}
 
-            let count = this.countData.transform(xhr);
+			let count = this.countData.transform(xhr);
 
-            if (count) {
-                os.innerHTML = count;
-            }
-        };
+			if (count) {
+				os.innerHTML = count;
+			}
+		};
 
-        xhr.open('GET', this.countData.url);
-        xhr.send();
-    }
+		xhr.open('GET', this.countData.url);
+		xhr.send();
+	}
 
-    // handle AJAX POST request
-    post(os) {
-        let xhr = new XMLHttpRequest();
+	// handle AJAX POST request
+	post(os) {
+		let xhr = new XMLHttpRequest();
 
-        // on success pass response to transform function
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState !== XMLHttpRequest.DONE ||
-                xhr.status !== 200) {
-                return;
-            }
+		// on success pass response to transform function
+		xhr.onreadystatechange = () => {
+			if (xhr.readyState !== XMLHttpRequest.DONE ||
+				xhr.status !== 200) {
+				return;
+			}
 
-            let count = this.countData.transform(xhr);
+			let count = this.countData.transform(xhr);
 
-            if (count) {
-                os.innerHTML = count;
-            }
-        };
+			if (count) {
+				os.innerHTML = count;
+			}
+		};
 
-        xhr.open('POST', this.countData.url);
-        xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-        xhr.send(JSON.stringify(this.countData.data));
-    }
+		xhr.open('POST', this.countData.url);
+		xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+		xhr.send(JSON.stringify(this.countData.data));
+	}
 
-    storeSet(type, count = 0) {
-        if (!window.localStorage || !type) {
-            return;
-        }
+	storeSet(type, count = 0) {
+		if (!window.localStorage || !type) {
+			return;
+		}
 
-        localStorage.setItem(`OpenShare-${type}`, count);
-    }
+		localStorage.setItem(`OpenShare-${type}`, count);
+	}
 
-    storeGet(type) {
-        if (!window.localStorage || !type) {
-            return;
-        }
+	storeGet(type) {
+		if (!window.localStorage || !type) {
+			return;
+		}
 
-        return localStorage.getItem(`OpenShare-${type}`);
-    }
+		return localStorage.getItem(`OpenShare-${type}`);
+	}
 
-    // facebook count data
-    facebook(url) {
-        return {
-            type: 'get',
-            url: `http://graph.facebook.com/?id=${url}`,
-            transform: (xhr) => {
-                let count = JSON.parse(xhr.responseText).shares;
-                this.storeSet(this.type, count);
-                return count;
-            }
-        };
-    }
+	// facebook count data
+	facebook(url) {
+		return {
+			type: 'get',
+			url: `http://graph.facebook.com/?id=${url}`,
+			transform: (xhr) => {
+				let count = JSON.parse(xhr.responseText).shares;
+				this.storeSet(this.type, count);
+				return count;
+			}
+		};
+	}
 
-    // pinterest count data
-    pinterest(url) {
-        return {
-            type: 'jsonp',
-            url: `http://api.pinterest.com/v1/urls/count.json?callback=?&url=${url}`,
-            transform: (data) => {
-                let count = data.count;
-                this.storeSet(this.type, count);
-                return count;
-            }
-        };
-    }
+	// pinterest count data
+	pinterest(url) {
+		return {
+			type: 'jsonp',
+			url: `http://api.pinterest.com/v1/urls/count.json?callback=?&url=${url}`,
+			transform: (data) => {
+				let count = data.count;
+				this.storeSet(this.type, count);
+				return count;
+			}
+		};
+	}
 
-    // linkedin count data
-    linkedin(url) {
-        return {
-            type: 'jsonp',
-            url: `http://www.linkedin.com/countserv/count/share?url=${url}&format=jsonp&callback=?`,
-            transform: (data) => {
-                let count = data.count;
-                this.storeSet(this.type, count);
-                return count;
-            }
-        };
-    }
+	// linkedin count data
+	linkedin(url) {
+		return {
+			type: 'jsonp',
+			url: `http://www.linkedin.com/countserv/count/share?url=${url}&format=jsonp&callback=?`,
+			transform: (data) => {
+				let count = data.count;
+				this.storeSet(this.type, count);
+				return count;
+			}
+		};
+	}
 
-    // reddit count data
-    reddit(url) {
-        return {
-            type: 'get',
-            url: `https://www.reddit.com/api/info.json?url=${url}`,
-            transform: (xhr) => {
-                let posts = JSON.parse(xhr.responseText).data.children,
-                    ups = 0;
+	// reddit count data
+	reddit(url) {
+		return {
+			type: 'get',
+			url: `https://www.reddit.com/api/info.json?url=${url}`,
+			transform: (xhr) => {
+				let posts = JSON.parse(xhr.responseText).data.children,
+					ups = 0;
 
-                posts.forEach((post) => {
-                    ups += Number(post.data.ups);
-                });
+				posts.forEach((post) => {
+					ups += Number(post.data.ups);
+				});
 
-                this.storeSet(this.type, ups);
+				this.storeSet(this.type, ups);
 
-                return ups;
-            }
-        };
-    }
+				return ups;
+			}
+		};
+	}
 
-    // linkedin count data
-    google(url) {
-        return {
-            type: 'post',
-            data: {
-                method: 'pos.plusones.get',
-                id: 'p',
-                params: {
-                    nolog: true,
-                    id: url,
-                    source: 'widget',
-                    userId: '@viewer',
-                    groupId: '@self'
-                },
-                jsonrpc: '2.0',
-                key: 'p',
-                apiVersion: 'v1'
-            },
-            url: `https://clients6.google.com/rpc`,
-            transform: (xhr) => {
-                let count = JSON.parse(xhr.responseText).result.metadata.globalCounts.count;
-                this.storeSet(this.type, count);
-                return count;
-            }
-        };
-    }
+	// linkedin count data
+	google(url) {
+		return {
+			type: 'post',
+			data: {
+				method: 'pos.plusones.get',
+				id: 'p',
+				params: {
+					nolog: true,
+					id: url,
+					source: 'widget',
+					userId: '@viewer',
+					groupId: '@self'
+				},
+				jsonrpc: '2.0',
+				key: 'p',
+				apiVersion: 'v1'
+			},
+			url: `https://clients6.google.com/rpc`,
+			transform: (xhr) => {
+				let count = JSON.parse(xhr.responseText).result.metadata.globalCounts.count;
+				this.storeSet(this.type, count);
+				return count;
+			}
+		};
+	}
 
 };
--- src/modules/data-attr.js
+++ src/modules/data-attr.js
@@ -1,117 +1,117 @@
 /**
-* Configure data attribute API
-*/
+ * Configure data attribute API
+ */
 module.exports = class DataAttr {
 
-    constructor(OpenShare, Count) {
-        this.OpenShare = OpenShare;
-        this.Count = Count;
+	constructor(OpenShare, Count) {
+		this.OpenShare = OpenShare;
+		this.Count = Count;
 
-        document.addEventListener('open-share-init', this.init.bind(this));
-        this.init();
-    }
+		document.addEventListener('open-share-init', this.init.bind(this));
+		this.init();
+	}
 
-    init() {
-        this.initializeNodes();
+	init() {
+		this.initializeNodes();
 
-        // check for mutation observers before using, IE11 only
-        if (window.MutationObserver !== undefined) {
-            this.initializeWatcher(document.querySelectorAll('[data-open-share-watch]'));
-        }
-    }
+		// check for mutation observers before using, IE11 only
+		if (window.MutationObserver !== undefined) {
+			this.initializeWatcher(document.querySelectorAll('[data-open-share-watch]'));
+		}
+	}
 
-    initializeNodes(container = document) {
-        // loop through open share node collection
-        let shareNodes = container.querySelectorAll('[data-open-share]:not([data-open-share-node])');
-        [].forEach.call(shareNodes, this.initializeShareNode.bind(this));
+	initializeNodes(container = document) {
+		// loop through open share node collection
+		let shareNodes = container.querySelectorAll('[data-open-share]:not([data-open-share-node])');
+		[].forEach.call(shareNodes, this.initializeShareNode.bind(this));
 
-        // loop through count node collection
-        let countNodes = container.querySelectorAll('[data-open-share-count]:not([data-open-share-node])');
-        [].forEach.call(countNodes, this.initializeCountNode.bind(this));
-    }
+		// loop through count node collection
+		let countNodes = container.querySelectorAll('[data-open-share-count]:not([data-open-share-node])');
+		[].forEach.call(countNodes, this.initializeCountNode.bind(this));
+	}
 
-    initializeCountNode(os) {
-        // initialize open share object with type attribute
-        let type = os.getAttribute('data-open-share-count'),
-            count = new this.Count(type, os.getAttribute('data-open-share-count-url'));
+	initializeCountNode(os) {
+		// initialize open share object with type attribute
+		let type = os.getAttribute('data-open-share-count'),
+			count = new this.Count(type, os.getAttribute('data-open-share-count-url'));
 
-        count.getCount(os);
-        os.setAttribute('data-open-share-node', type);
-    }
+		count.getCount(os);
+		os.setAttribute('data-open-share-node', type);
+	}
 
-    initializeShareNode(os) {
-        // initialize open share object with type attribute
-        let type = os.getAttribute('data-open-share'),
-            dash = type.indexOf('-'),
-            openShare;
+	initializeShareNode(os) {
+		// initialize open share object with type attribute
+		let type = os.getAttribute('data-open-share'),
+			dash = type.indexOf('-'),
+			openShare;
 
-        // type contains a dash
-        // transform to camelcase for function reference
-        // TODO: only supports single dash, should should support multiple
-        if (dash > -1) {
-            let nextChar = type.substr(dash + 1, 1),
-                group = type.substr(dash, 2);
+		// type contains a dash
+		// transform to camelcase for function reference
+		// TODO: only supports single dash, should should support multiple
+		if (dash > -1) {
+			let nextChar = type.substr(dash + 1, 1),
+				group = type.substr(dash, 2);
 
-            type = type.replace(group, nextChar.toUpperCase());
-        }
+			type = type.replace(group, nextChar.toUpperCase());
+		}
 
-        openShare = new this.OpenShare(type);
+		openShare = new this.OpenShare(type);
 
-        // specify if this is a dynamic instance
-        if (os.getAttribute('data-open-share-dynamic')) {
-            openShare.dynamic = true;
-        }
+		// specify if this is a dynamic instance
+		if (os.getAttribute('data-open-share-dynamic')) {
+			openShare.dynamic = true;
+		}
 
-        // set all optional attributes on open share instance
-        this.setData(openShare, os);
+		// set all optional attributes on open share instance
+		this.setData(openShare, os);
 
-        // open share dialog on click
-        os.addEventListener('click', (e) => {
+		// open share dialog on click
+		os.addEventListener('click', (e) => {
 
-            // if dynamic instance then fetch attributes again in case of updates
-            if (openShare.dynamic) {
-                this.setData(openShare, e.currentTarget);
-            }
+			// if dynamic instance then fetch attributes again in case of updates
+			if (openShare.dynamic) {
+				this.setData(openShare, e.currentTarget);
+			}
 
-            openShare.share(e);
-        });
+			openShare.share(e);
+		});
 
-        os.setAttribute('data-open-share-node', type);
-    }
+		os.setAttribute('data-open-share-node', type);
+	}
 
-    initializeWatcher(watcher) {
-        [].forEach.call(watcher, (w) => {
-            var observer = new MutationObserver((mutations) => {
-                // target will match between all mutations so just use first
-                this.initializeNodes(mutations[0].target);
-            });
+	initializeWatcher(watcher) {
+		[].forEach.call(watcher, (w) => {
+			var observer = new MutationObserver((mutations) => {
+				// target will match between all mutations so just use first
+				this.initializeNodes(mutations[0].target);
+			});
 
-            observer.observe(w, {
-                childList: true
-            });
-        });
-    }
+			observer.observe(w, {
+				childList: true
+			});
+		});
+	}
 
-    setData(osInstance, osElement) {
-        osInstance.setData({
-            url: osElement.getAttribute('data-open-share-url'),
-            text: osElement.getAttribute('data-open-share-text'),
-            via: osElement.getAttribute('data-open-share-via'),
-            hashtags: osElement.getAttribute('data-open-share-hashtags'),
-            ios: osElement.getAttribute('data-open-share-ios'),
-            tweetId: osElement.getAttribute('data-open-share-tweet-id'),
-            related: osElement.getAttribute('data-open-share-related'),
-            screenName: osElement.getAttribute('data-open-share-screen-name'),
-            userId: osElement.getAttribute('data-open-share-user-id'),
-            link: osElement.getAttribute('data-open-share-link'),
-            picture: osElement.getAttribute('data-open-share-picture'),
-            caption: osElement.getAttribute('data-open-share-caption'),
-            description: osElement.getAttribute('data-open-share-description'),
-            title: osElement.getAttribute('data-open-share-title'),
-            media: osElement.getAttribute('data-open-share-media'),
-            to: osElement.getAttribute('data-open-share-to'),
-            subject: osElement.getAttribute('data-open-share-subject'),
-            body: osElement.getAttribute('data-open-share-body')
-        });
-    }
+	setData(osInstance, osElement) {
+		osInstance.setData({
+			url: osElement.getAttribute('data-open-share-url'),
+			text: osElement.getAttribute('data-open-share-text'),
+			via: osElement.getAttribute('data-open-share-via'),
+			hashtags: osElement.getAttribute('data-open-share-hashtags'),
+			ios: osElement.getAttribute('data-open-share-ios'),
+			tweetId: osElement.getAttribute('data-open-share-tweet-id'),
+			related: osElement.getAttribute('data-open-share-related'),
+			screenName: osElement.getAttribute('data-open-share-screen-name'),
+			userId: osElement.getAttribute('data-open-share-user-id'),
+			link: osElement.getAttribute('data-open-share-link'),
+			picture: osElement.getAttribute('data-open-share-picture'),
+			caption: osElement.getAttribute('data-open-share-caption'),
+			description: osElement.getAttribute('data-open-share-description'),
+			title: osElement.getAttribute('data-open-share-title'),
+			media: osElement.getAttribute('data-open-share-media'),
+			to: osElement.getAttribute('data-open-share-to'),
+			subject: osElement.getAttribute('data-open-share-subject'),
+			body: osElement.getAttribute('data-open-share-body')
+		});
+	}
 };
