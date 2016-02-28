0084e6a9242dfeb37e7451ff6b7e01eb56a380ee
Adam Chambers <adammchambers@gmail.com>
2016-02-28 16:25:47 -0500

    Basic JS API example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 (function() {
 
 	var DataAttr = require('./modules/data-attr'),
+		API = require('./modules/api'),
 		OpenShare = require('./modules/open-share'),
 		Transforms = require('./modules/transforms'),
 		Count = require('./modules/count');
 
 	new DataAttr(OpenShare, Transforms, Count);
+	new API(OpenShare, Transforms);
 })();
---
+++ src/modules/api.js
@@ -0,0 +1,25 @@
+module.exports = function(OS, Transforms) {
+
+	function OpenShare(element, data) {
+		var os = new OS(data.type, Transforms[data.type]);
+		os.setData(data);
+
+		// open share dialog on click
+		element.addEventListener('click', (e) => {
+
+			// if dynamic instance then fetch attributes again in case of updates
+			if (data.dynamic) {
+				os.setData(data);
+			}
+
+			os.share(e);
+
+			// trigger shared event
+			let sharedEvent = document.createEvent('Event');
+			sharedEvent.initEvent('OpenShare.shared', true, true);
+			element.dispatchEvent(sharedEvent);
+		});
+	}
+
+	window.OpenShare = OpenShare;
+};
