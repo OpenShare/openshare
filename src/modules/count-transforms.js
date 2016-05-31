44f8595738ff053de5c7d524ec162ba14370b9b3
jekrb <Jacob.JW.Burden@gmail.com>
2016-05-31 16:19:13 -0400

    M and K

--- src/modules/count-transforms.js
+++ src/modules/count-transforms.js
@@ -156,9 +156,7 @@ module.exports = {
 					let page = 2;
 					recursiveCount(url, page, count, finalCount => {
 						this.storeSet(this.type + '-' + this.shared, finalCount);
-						this.os.innerHTML = finalCount > 999 ?
-							Math.round(finalCount/1000) + 'k' :
-							finalCount;
+						countReduce(this.os, finalCount);
 						Events.trigger(this.os, 'counted-' + this.url);
 						return finalCount;
 					});
@@ -188,4 +186,34 @@ function recursiveCount (url, page, count, cb) {
 		}
 	});
 	xhr.send();
+}
+
+function round(x, precision) {
+	if (typeof x !== 'number') {
+		throw new TypeError('Expected value to be a number');
+	}
+
+	var exponent = precision > 0 ? 'e' : 'e-';
+	var exponentNeg = precision > 0 ? 'e-' : 'e';
+	precision = Math.abs(precision);
+
+	return Number(Math.round(x + exponent + precision) + exponentNeg + precision);
+}
+
+function thousandify (num) {
+	return round(num/1000, 1) + 'K';
+}
+
+function millionify (num) {
+	return round(num/1000000, 1) + 'M';
+}
+
+function countReduce (el, count) {
+	if (count > 999999)  {
+		el.innerHTML = millionify(count);
+	} else if (count > 999) {
+		el.innerHTML = thousandify(count);
+	} else {
+		el.innerHTML = count;
+	}
 }
