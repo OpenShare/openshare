8888123c796681206be5b1f00b33d889bb690a90
jekrb <Jacob.JW.Burden@gmail.com>
2016-06-08 00:27:41 -0400

    Lets pause the masthead openshare node transforms on mousover and start again on mouseout from where we left off.

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -15,13 +15,12 @@ function animationLoop() {
     animationMods.forEach(function(mod, i, arr) {
 
         // wait a second in between each animation segment
-        setTimeout(function() {
+    	var timer = new Timer(function() {
 
             // loop through open share nodes
             [].forEach.call(openShareNodes, function(node, j) {
-
                 // delay by index * 100ms
-                setTimeout(function() {
+                var delay = new Timer(function() {
 
                     // out of mods so reset
                     if (!mod) {
@@ -34,14 +33,72 @@ function animationLoop() {
 
                     // bind node to setTimeout so reference doesn't change on each loop
                 }.bind(node), j * 100);
+
             });
         }, i * 1000);
+
+		[].forEach.call(openShareNodes, function(node) {
+			node.addEventListener('mouseenter', function () {
+				timer.pause();
+			});
+			node.addEventListener('mouseleave', function () {
+				timer.resume();
+			});
+		});
     });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    setInterval(animationLoop, 6000);
+    var interval = new RecurringTimer(animationLoop, 6000);
+	[].forEach.call(openShareNodes, function(node) {
+		node.addEventListener('mouseenter', function () {
+			interval.pause();
+		});
+		node.addEventListener('mouseleave', function () {
+			interval.resume();
+		});
+	});
     setTimeout(function() {
 		animationLoop();
 	}, 1000);
 });
+
+function Timer(callback, delay) {
+    var timerId, start, remaining = delay;
+
+    this.pause = function() {
+        window.clearTimeout(timerId);
+        remaining -= new Date() - start;
+    };
+
+    this.resume = function() {
+        start = new Date();
+        window.clearTimeout(timerId);
+        timerId = window.setTimeout(callback, remaining);
+    };
+
+    this.resume();
+}
+
+
+function RecurringTimer(callback, delay) {
+    var timerId, start, remaining = delay;
+
+    this.pause = function() {
+        window.clearTimeout(timerId);
+        remaining -= new Date() - start;
+    };
+
+    var resume = function() {
+        start = new Date();
+        timerId = window.setTimeout(function() {
+            remaining = delay;
+            resume();
+            callback();
+        }, remaining);
+    };
+
+    this.resume = resume;
+
+    this.resume();
+}
