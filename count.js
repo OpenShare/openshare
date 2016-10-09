af9256073660ba5e676d42c856352bbb40fc875b
Iswan Jumat <iswan@thinkcube.com>
2016-10-09 20:28:02 +0530

    added eslintignore and fixes few eslint issues

--- count.js
+++ count.js
@@ -1,9 +1,9 @@
-module.exports = (function() {
-	document.addEventListener('DOMContentLoaded', require('./lib/init')({
-		api: 'count',
-		selector: '[data-open-share-count]:not([data-open-share-node])',
-		cb: require('./lib/initializeCountNode')
-	}));
+module.exports = (function () { // eslint-disable-line
+  document.addEventListener('DOMContentLoaded', require('./lib/init')({
+    api: 'count',
+    selector: '[data-open-share-count]:not([data-open-share-node])',
+    cb: require('./lib/initializeCountNode'),
+  }));
 
-	return require('./src/modules/count-api')();
-})();
+  return require('./src/modules/count-api')();
+}());
--- share.js
+++ share.js
@@ -1,9 +1,9 @@
-module.exports = (function() {
-	document.addEventListener('DOMContentLoaded', require('./lib/init')({
-		api: 'share',
-		selector: '[data-open-share]:not([data-open-share-node])',
-		cb: require('./lib/initializeShareNode')
-	}));
+module.exports = (function () { // eslint-disable-line
+  document.addEventListener('DOMContentLoaded', require('./lib/init')({
+    api: 'share',
+    selector: '[data-open-share]:not([data-open-share-node])',
+    cb: require('./lib/initializeShareNode'),
+  }));
 
-	return require('./src/modules/share-api')();
-})();
+  return require('./src/modules/share-api')();
+}());
